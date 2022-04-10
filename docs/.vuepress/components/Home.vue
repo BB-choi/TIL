a609bed6be1e65cac7b7f3e220717cfcb2c24312
BB-choi <choibobae0809@gmail.com>
2022-04-10 22:31:27 +0900

    TIL 스타일 관련 변경사항
    - 파비콘 추가
    - lastUpdated transparent color로 설정
    - 새로운 카테고리 추가(config.js)
    - 다크모드 색상 설정

--- docs/.vuepress/components/Home.vue
+++ docs/.vuepress/components/Home.vue
@@ -4,7 +4,7 @@
       <h1>Today<br />BB<br />Learned</h1>
     </div>
     <div class="info-area">
-      <p>✍(‘•̀ ▽ •́ ) 오늘 공부한 것을 기록합니다</p>
+      <p>꾸준하게 🐢🐢🐢<br />✍(‘•̀ ▽ •́ ) 오늘 공부한 것을 기록합니다</p>
     </div>
   </div>
 </template>
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,6 +4,7 @@ module.exports = {
   title: "TIL",
   description: "Today I Learned",
   base: "/TIL/",
+  head: [["link", { rel: "icon", href: `/images/logo-144.png` }]],
   themeConfig: {
     nav: [{ text: "GitHub", link: "https://github.com/bb-choi" }],
     lastUpdated: "마지막 수정일",
@@ -11,6 +12,10 @@ module.exports = {
       {
         title: "2022-04",
         children: util.getFileArr("til", "22-04"),
+      },
+      {
+        title: "Javascript",
+        children: util.getFileArr("javascript"),
       },
     ],
   },
