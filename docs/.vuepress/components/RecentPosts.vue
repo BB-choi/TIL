3ecc4e7f7c7741290995db5090ac45189e2b9bb2
BB-choi <choibobae0809@gmail.com>
2022-07-18 21:22:52 +0900

    최근 글 표시 방법 변경(#1)
    - tag 이용

--- docs/.vuepress/components/RecentPosts.vue
+++ docs/.vuepress/components/RecentPosts.vue
@@ -10,19 +10,43 @@
 </template>
 
 <script>
-const POST_COUNT = 5;
 export default {
   data() {
     return {};
   },
   computed: {
     recentPosts() {
-      const posts = this.$site.pages
-        .filter((post) => post.path !== "/")
-        .sort((a, b) => new Date(b.lastUpdated) - new Date(a.lastUpdated))
+      const POST_COUNT = 5;
+      const now = new Date();
+
+      const getMonthTag = (date) => {
+        return date.toISOString().slice(2, 7); // "22-07"
+      };
+
+      const getRecentPosts = (month) => {
+        return this.$site.pages.filter((page) =>
+          page.frontmatter.tags?.includes(month)
+        );
+      };
+
+      const thisMonth = getMonthTag(now);
+      let recentPosts = getRecentPosts(thisMonth);
+
+      const year = now.getFullYear();
+      const month = now.getMonth();
+      const day = now.getDate();
+
+      while (!recentPosts.length) {
+        recentPosts = getRecentPosts(
+          new Date(year, month - 1, day).toISOString().slice(2, 7)
+        );
+      }
+
+      const slicedRecentPosts = recentPosts
+        .sort(({ title: a }, { title: b }) => b.localeCompare(a))
         .slice(0, POST_COUNT);
 
-      return posts;
+      return slicedRecentPosts;
     },
   },
 };
--- docs/.vuepress/util.js
+++ docs/.vuepress/util.js
@@ -10,18 +10,4 @@ module.exports = {
       (el) => `${dir1}${dir2 ? "/" + dir2 : ""}/${el.toString()}`
     );
   },
-  getThisMonthArrDesc: () => {
-    const thisMonth = "2022-07";
-    const thisMonthPages = [];
-
-    for (const page of this.$site.pages) {
-      for (const idx in page.frontmatter.tags) {
-        if (page.frontmatter.tags[idx] === thisMonth) {
-          thisMonthPages.push(page);
-        }
-      }
-    }
-
-    return thisMonthPages.sort((a, b) => b.localeCompare(a));
-  },
 };
